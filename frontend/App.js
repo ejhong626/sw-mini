212f5b0a56c89153f28a4be1791f5b07fe10daf5
evazcy0223 <56014930+evazcy0223@users.noreply.github.com>
2021-09-17 04:04:31 -0400

    Add files via upload

---
+++ frontend/App.js
@@ -0,0 +1,86 @@
+import { StatusBar } from 'expo-status-bar';
+import React, { Component } from 'react';
+import { StyleSheet, TouchableOpacity, Text, View, Platform } from 'react-native';
+
+import { NavigationContainer, DefaultTheme, DarkTheme } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import LogScreen from './screens/login'; 
+import MainScreen from './screens/main';
+import RecipeScreen from './screens/recipe';
+import ScanScreen from './screens/scan';
+
+
+const Stack = createNativeStackNavigator(); 
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Welcome!</Text>
+      <TouchableOpacity
+       style={styles.button}
+       onPress={() => navigation.navigate("Log In")}
+      >
+       <Text>Log In</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+       style={styles.button}
+       onPress={() => navigation.navigate("Registration")}
+      >
+       <Text>Register</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+
+const RegisterScreen = ({navigation}) =>{
+  return(
+    <View style={styles.container}>
+      <Text> Registration </Text>
+    </View>
+  )
+}
+
+const App = () =>{
+    return (
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name='Home' component={HomeScreen}/>
+          <Stack.Screen name='Log In' component={LogScreen}/>
+          <Stack.Screen name='Registration' component={RegisterScreen}/>
+          <Stack.Screen name='Main' component={MainScreen}/>
+          <Stack.Screen name='Recipe' component={RecipeScreen}/>
+          <Stack.Screen name='Scan' component={ScanScreen}/>
+          
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: Platform.OS === "android" ? 20:0,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  button: {
+    alignItems: 'center',
+    backgroundColor: '#DDDDDD',
+    padding: 10,
+    paddingHorizontal: 60,
+    marginBottom: 10,
+  },
+  title: {
+    marginTop: 16,
+    paddingVertical: 8,
+    color: "#20232a",
+    textAlign: "center",
+    fontSize: 30,
+    fontWeight: "bold"
+  }
+});
+
+export default App;
